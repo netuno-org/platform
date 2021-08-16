9b732ea52da75cca4690cd5945f4a94d0ea0ed49
eduardo.velasques <eduardo.velasques@sitana.pt>
2021-08-16 13:11:36 +0100

    Update scripts with GitHub url.

--- bundle/base/update.bat
+++ bundle/base/update.bat
@@ -1,5 +1,5 @@
 
-powershell -Command "Invoke-WebRequest -Uri https://www.netuno.org/netuno.jar -OutFile netuno.jar"
+powershell -Command "Invoke-WebRequest -Uri https://github.com/netuno-org/platform/releases/download/latest/netuno.jar -OutFile netuno.jar"
 
 java -jar netuno.jar install
 
--- bundle/base/update.sh
+++ bundle/base/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl https://www.netuno.org/netuno.jar > netuno.jar
+curl -L https://github.com/netuno-org/platform/releases/download/latest/netuno.jar -o netuno.jar
 
 java -jar netuno.jar install
 
