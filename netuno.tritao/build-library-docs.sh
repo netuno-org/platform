88e46026dd776ea0c193b0d4cf4e871098cca647
eduardo.velasques <eduardo.velasques@sitana.pt>
2021-08-19 17:06:32 +0100

    The script to build the Tritão resources documentation has been fixed to a new path.

--- netuno.tritao/build-library-docs.sh
+++ netuno.tritao/build-library-docs.sh
@@ -2,15 +2,15 @@
 
 mvn -Dtest=BuildLibraryTest test
 
-trash-put ../netuno.docs/docs/library/objects
-trash-put ../netuno.docs/docs/library/resources
+trash-put ../../doc/docs/library/objects
+trash-put ../../doc/docs/library/resources
 
-trash-put ../netuno.docs/website/translated_docs/pt-PT/library/objects
-trash-put ../netuno.docs/website/translated_docs/pt-PT/library/resources
+trash-put ../../doc/website/translated_docs/pt-PT/library/objects
+trash-put ../../doc/website/translated_docs/pt-PT/library/resources
 
-cp -r docs/EN/library/objects ../netuno.docs/docs/library/objects
-cp -r docs/EN/library/resources ../netuno.docs/docs/library/resources
+cp -r docs/EN/library/objects ../../doc/docs/library/objects
+cp -r docs/EN/library/resources ../../doc/docs/library/resources
 
-cp -r docs/PT/library/objects ../netuno.docs/website/translated_docs/pt-PT/library/objects
-cp -r docs/PT/library/resources ../netuno.docs/website/translated_docs/pt-PT/library/resources
+cp -r docs/PT/library/objects ../../doc/website/translated_docs/pt-PT/library/objects
+cp -r docs/PT/library/resources ../../doc/website/translated_docs/pt-PT/library/resources
 
