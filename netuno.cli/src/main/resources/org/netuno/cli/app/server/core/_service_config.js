b6b5ebd227bd644a305e12cfc89db4aa64ab0a0a
eduardo.velasques <eduardo.velasques@sitana.pt>
2022-03-07 00:07:41 +0000

    The Base App UI with all dependencies updated.

--- netuno.cli/src/main/resources/org/netuno/cli/app/server/core/_service_config.js
+++ netuno.cli/src/main/resources/org/netuno/cli/app/server/core/_service_config.js
@@ -3,11 +3,11 @@
  *  When service need public access...
  */
 if (_env.is("dev")) {
-    _service.allow()
+  _service.allow()
 }
 
 /*
-if (_service.path == 'samples/my-service') {
+  if (_service.path == 'samples/my-service') {
     _service.allow()
-}
+  }
 */
