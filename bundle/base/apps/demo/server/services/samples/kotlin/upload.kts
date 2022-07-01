8671e39a1db3c58c8c63e00d9a48ac707292536b
Eduardo Velasques <eduveks@gmail.com>
2022-07-01 12:28:28 +0000

    App demo with upload sample extracting data from XLSX and PDF files.

--- bundle/base/apps/demo/server/services/samples/kotlin/upload.kts
+++ bundle/base/apps/demo/server/services/samples/kotlin/upload.kts
@@ -3,18 +3,24 @@ val data = _val.init()
 
 var filePath = ""
 
-if (_req.hasKey("fileToUpload")) {
-    val file = _req.getFile("fileToUpload")
+if (_req.hasKey("fileUpload")) {
+    val file = _req.getFile("fileUpload")
     val storage = _storage.filesystem("server", "samples", file.getName())
     file.save(storage)
     filePath = storage.fullPath()
-    data.set("serverAppFilePath", filePath)
+    data.set("filePath", filePath)
+
+    if (arrayOf("xls", "xlsx").contains(file.extension().toLowerCase())) {
+        val xlsInfo = _xls.read(file)
+        data.set("extra", xlsInfo.toJSON(true, 4))
+    } else if ("pdf" == file.extension().toLowerCase()) {
+        val pdfInfo = _pdf.extract(storage)
+        data.set("extra", pdfInfo.getString("content"))
+    }
 }
 
 if (_req.getBoolean("ajax")) {
-    _out.json(hashMapOf(
-        "filePath" to filePath
-    ))
+    _out.json(data)
 } else {
     _template.output("samples/upload", data)
 }
