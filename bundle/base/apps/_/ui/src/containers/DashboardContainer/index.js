791199d181c818f07596ac3d9d916db9032af721
Eduardo Velasques <eduveks@gmail.com>
2023-03-22 22:33:07 +0000

    Bundle: Application base with dependencies updated and adapted to new Ant.Design v5.

--- bundle/base/apps/_/ui/src/containers/DashboardContainer/index.js
+++ bundle/base/apps/_/ui/src/containers/DashboardContainer/index.js
@@ -1,8 +1,22 @@
 import React, { useState, useEffect, useRef } from "react";
 
+import { ConfigProvider, theme } from "antd";
+
+import antLocale_enGB from "antd/lib/locale/en_GB";
+import antLocale_enUS from "antd/lib/locale/en_US";
+import antLocale_esES from "antd/lib/locale/es_ES";
+import antLocale_ptBR from "antd/lib/locale/pt_BR";
+import antLocale_ptPT from "antd/lib/locale/pt_PT";
+
 import MyButton from "../../components/MyButton";
 
 import "./index.less";
+
+const antdVariables = {
+  colorPrimary: '#5b5ce1',
+  colorLink: '#5b5ce1',
+  borderRadius: 5,
+};
 
 function DashboardContainer() {
   const [counter, setCounter] = useState(0);
@@ -18,11 +32,27 @@ function DashboardContainer() {
   };
 
   return (
-    <div className="my-dashboard">
-      <div ref={refButton} className="my-dashboard__button">
-        <MyButton text={`ReactJS ⚡ Ant.Design 👉 Click me! ${counter}`} onClick={onClick} />
+    <ConfigProvider
+      theme={{
+        token: antdVariables,
+        algorithm: theme.darkAlgorithm
+      }}
+      locale={
+        {
+          'en_us': antLocale_enUS,
+          'en_gb': antLocale_enGB,
+          'es_es': antLocale_esES,
+          'pt_br': antLocale_ptBR,
+          'pt_pt': antLocale_ptPT
+        }[netuno.config.langCode]
+      }
+    >
+      <div className="my-dashboard">
+        <div ref={refButton} className="my-dashboard__button">
+          <MyButton text={`ReactJS ⚡ Ant.Design 👉 Click me! ${counter}`} onClick={onClick} />
+        </div>
       </div>
-    </div>
+    </ConfigProvider>
   );
 }
 
