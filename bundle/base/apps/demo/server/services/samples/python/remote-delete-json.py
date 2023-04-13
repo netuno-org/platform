f0fe1a4ec8a209b4a1d0a6047dcd3857a05587b8
Eduardo Velasques <eduveks@gmail.com>
2023-04-13 22:42:04 +0000

    Bundle: Demo app with python samples to exports and remotes.

---
+++ bundle/base/apps/demo/server/services/samples/python/remote-delete-json.py
@@ -0,0 +1,21 @@
+
+#
+#  EN: Service REST with DELETE
+#  EN: Let's call an external REST service with the method
+#  EN: post and send a JSON.
+#
+#  PT: Serviço REST com DELETE
+#  PT: Vamos chamar um serviço REST externo com o método
+#  PT: delete e enviar um JSON.
+#
+
+client = _remote.init()
+
+response = client.asJSON().delete(
+    "http://httpbin.org/delete",
+    _val.init()
+        .set("name", "morpheus")
+        .set("job", "leader")
+)
+
+_out.json(response)
---
+++ bundle/base/apps/demo/server/services/samples/python/remote-get-json.py
@@ -0,0 +1,17 @@
+
+#
+#  EN: CALL REMOTE JSON SERVICE
+#  EN: Is very simple to call others services like REST, supports all HTTP methods.
+#
+#  PT: EXECUTA UM SERVIÇO JSON REMOTAMENTE
+#  PT: É muito simples chamar outros serviços como REST, suporta todos os métodos HTTP.
+#
+
+response = _remote.init().get("https://api.ipify.org?format=json")
+
+if response.ok():
+    if response.isJSON():
+        _out.json(
+            response
+        )
+
