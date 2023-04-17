6ea4d1210af99294cc0fb5788e45ae2f0860b8dd
Eduardo Velasques <eduveks@gmail.com>
2023-04-17 00:37:58 +0000

    Bundle: Demo app with sample of the image created programmatically.

--- bundle/base/apps/demo/server/services/samples/javascript/image.js
+++ bundle/base/apps/demo/server/services/samples/javascript/image.js
@@ -1,31 +1,27 @@
 
+/**
+ *
+ *  EN: IMAGE PROCESSING
+ *  EN: Example of the programmatically created image.
+ *
+ *  PT: PROCESSAMENTO DE IMAGEM
+ *  PT: Exemplo de imagem criada programaticamente.
+ *
+ */
+
 _header.contentType("png")
 
-var viksiScript = _image.font(_storage.filesystem("server", "samples/export-pdf", "viksi-script.ttf"), _image.fontField("plain"), 40.0);
-var robotomono = _image.font(_image.fontField("truetype-font"), _storage.filesystem("server", "samples/export-pdf", "robotomono-bold.ttf"), _image.fontField("plain"), 40.0);
+const viksiScriptFont = _image.font(_storage.filesystem("server", "samples/export-pdf", "viksi-script.ttf"), _image.fontField("plain"), 20.0);
 
-var vsf = _image.font(_storage.filesystem("server", "samples/export-pdf", "viksi-script.ttf"));
-var rm = _image.font(_storage.filesystem("server", "samples/export-pdf", "robotomono-bold.ttf"));
-vsf = vsf.deriveFont(10);
-rm = rm.deriveFont(0, _convert.toFloat(40));
-
-var logo = _image.init(_storage.filesystem("server", "samples/export-pdf", "logo.png"))
-
-var logoX2 = _image.init(_storage.filesystem("server", "samples/export-pdf", "logo.png"))
-
-
-logo.drawText(
-  "TESTE FONTE!!!",
-  robotomono,
-  _image.color("yellow"),
-  50, 100, 90
-);
+const logo = _image.init(_storage.filesystem("server", "samples/export-pdf", "logo.png"))
 
 logo.resize(500, 500)
 
 logo.crop(10, 10, 480, 480)
 
-logo.drawBackground(_image.color("white"))
+logo.fillRect(_image.color(0, 0, 0, 0.75), 0, 0, 500, 500)
+
+logo.drawBackground(_image.color("black"))
 
 logo.drawLine(_image.color("red"), 20, 20, 50, 50)
 
@@ -34,21 +30,42 @@ logo.fillRect(_image.color("orange"), 80, 20, 50, 50, -45)
 logo.fillOval(_image.color("red"), 140, 20, 50, 50)
 
 logo.fillPolygon(
-  _image.color("red"),
-  _val.list().add(150).add(200).add(200),
-  _val.list().add(150).add(200).add(100)
+  _image.color(1, 0, 0, 0.5),
+  _val.list().add(350).add(400).add(450).add(400),
+  _val.list().add(200).add(150).add(200).add(250)
 )
 
-
 logo.fillPolygon(
-  _image.color("orange"),
+  _image.color(1, 0.75, 0, 0.5),
   [50, 100, 100],
   [150, 200, 100]
 )
 
-logo.drawImage(logoX2.getImage(), 100, 200, 45)
+const logoOther = _image.init(_storage.filesystem("server", "samples/export-pdf", "logo.png"))
+logoOther.resize(logoOther.width() * 0.5, logoOther.height() * 0.5)
+logo.drawImage(logoOther.getImage(), 150, 180, 45)
+logo.drawImage(logoOther.getImage(), 150, 225, -15)
 
+logo.drawText(
+  "My Font Text 1!",
+  viksiScriptFont,
+  _image.color("cyan"),
+  50, 415 
+)
 
-//logo.drawText("OUTRA", _image.font("Default", _image.fontField("plain"), 40), _image.color("blue"), 10, 40);
+logo.drawText(
+  "My Font Text 2!",
+  viksiScriptFont,
+  _image.color("pink"),
+  425, 405, 180
+)
+
+logo.drawText(
+  "Default Text...",
+  _image.font("Default", _image.fontField("plain"), 40),
+  _image.color("green"),
+  50, 475
+)
 
 logo.output("png")
+
