abf9aa9bd40bdb5d9d15eba1784d76f7b480c344
Eduardo Velasques <eduveks@gmail.com>
2023-08-01 19:19:13 -0300

    Bundle: Base app has the Vite supported and dependencies updated.

--- bundle/base/apps/_/ui/src/index.jsx
+++ bundle/base/apps/_/ui/src/index.jsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import _service from '@netuno/service-client';
 import DashboardContainer from "./containers/DashboardContainer";
 
+import { ConfigProvider, theme } from "antd";
+import antLocale_enGB from "antd/lib/locale/en_GB";
+import antLocale_enUS from "antd/lib/locale/en_US";
+import antLocale_esES from "antd/lib/locale/es_ES";
+import antLocale_ptBR from "antd/lib/locale/pt_BR";
+import antLocale_ptPT from "antd/lib/locale/pt_PT";
+
 _service.config({
   prefix: netuno.config.urlServices
 });
@@ -12,7 +19,29 @@ const dashboardDiv = document.getElementById("app-dashboard");
 const dashboardContainer = dashboardDiv ? ReactDOM.createRoot(dashboardDiv) : false;
 
 if (dashboardContainer) {
-  dashboardContainer.render(<DashboardContainer/>);
+  dashboardContainer.render(
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#5b5ce1',
+          colorLink: '#5b5ce1',
+          borderRadius: 5,
+        },
+        algorithm: theme.darkAlgorithm
+      }}
+      locale={
+        {
+          'en_us': antLocale_enUS,
+          'en_gb': antLocale_enGB,
+          'es_es': antLocale_esES,
+          'pt_br': antLocale_ptBR,
+          'pt_pt': antLocale_ptPT
+        }[netuno.config.langCode]
+      }
+    >
+      <DashboardContainer/>
+    </ConfigProvider>
+  );
 }
 
 netuno.addNavigationLoad(() => {
