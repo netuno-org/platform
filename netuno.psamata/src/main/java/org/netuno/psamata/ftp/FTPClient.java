f368570506c6b9f08c20a1e728149c55910b7873
Eduardo Velasques <eduveks@gmail.com>
2023-12-28 12:51:53 +0000

    Psamata: FTP with license and others.

--- netuno.psamata/src/main/java/org/netuno/psamata/ftp/FTPClient.java
+++ netuno.psamata/src/main/java/org/netuno/psamata/ftp/FTPClient.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Netuno.org under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The Netuno.org licenses this file to You under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.netuno.psamata.ftp;
 
 import java.io.IOException;
@@ -15,6 +32,10 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.netuno.psamata.io.File;
 
+/**
+ * FTP Client
+ * @author Eduardo Fonseca Velasques - @eduveks
+ */
 public class FTPClient {
     private static Logger logger = LogManager.getLogger(FTPClient.class);
     private org.apache.commons.net.ftp.FTPClient ftpClient = null;
