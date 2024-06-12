cbe193eec6a76c6227e65d60773d6afce62133ee
JailtonAraujo <jailtonraneimoney@gmail.com>
2024-06-11 22:17:36 -0300

    Tritão: implements relational operators where clausure in QueryEngine

--- netuno.tritao/src/main/java/org/netuno/tritao/form/QueryEngine.java
+++ netuno.tritao/src/main/java/org/netuno/tritao/form/QueryEngine.java
@@ -8,6 +8,7 @@ import org.netuno.tritao.hili.Hili;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class QueryEngine extends Data {
 
@@ -52,6 +53,10 @@ public class QueryEngine extends Data {
         } else if (relation.getType().equals(RelationType.OneToOne)) {
             relationSQL += table+"."+relation.getColumn() + " = " + relation.getTableName()+".id";
         }
+        for(Map.Entry<String, Link> subRelationEntry : relation.getSubRelations().entrySet()) {
+            Link link = subRelationEntry.getValue();
+            relationSQL += this.buildLinkSQL(link);
+        }
         return " " + relationSQL;
     }
 
@@ -59,16 +64,33 @@ public class QueryEngine extends Data {
         String whereSQL = "";
         final String table = where.getTable();
         final Condition firstCondition = where.getFirstCondition();
-        if (firstCondition.getValue() instanceof String) {
-            whereSQL += " " + table+"."+firstCondition.getColumn() + " = " + "'"+firstCondition.getValue().toString()+"'";
-        } else {
-            whereSQL += " " + table+"."+firstCondition.getColumn() + " = " + firstCondition.getValue().toString();
-        }
+        whereSQL += this.buildRelationOperatorSQL(firstCondition.getRelationOperator(), table, firstCondition.getColumn());
         for (Map.Entry<String, Condition> conditionEntry : where.getConditions().entrySet()) {
             Condition condition = conditionEntry.getValue();
             whereSQL += buildCondition(condition, table);
         }
         return whereSQL;
+    }
+
+    public String buildRelationOperatorSQL(RelationOperator relationOperator, String table, String column) {
+        String relationOperatorSQL = "";
+        if (relationOperator.getOperatorType().equals(RelationOperatorType.Equals)) {
+            if (relationOperator.getValue() instanceof String) {
+                relationOperatorSQL = " " + table+"."+column + " = " + "'"+relationOperator.getValue().toString()+"'";
+            } else {
+                relationOperatorSQL = " " + table+"."+column + " = " + relationOperator.getValue().toString();
+            }
+        } else if (relationOperator.getOperatorType().equals(RelationOperatorType.StartsWith)) {
+            relationOperatorSQL = " " + "LOWER(" +table+"."+column+ ")" + " LIKE " + "LOWER('"+relationOperator.getValue().toString()+"%')";
+        } else if (relationOperator.getOperatorType().equals(RelationOperatorType.EndsWith)) {
+            relationOperatorSQL = " " + "LOWER(" +table+"."+column+ ")" + " LIKE " + "LOWER('%"+relationOperator.getValue().toString()+"')";
+        } else if (relationOperator.getOperatorType().equals(RelationOperatorType.Contains)) {
+            relationOperatorSQL = " " + "LOWER(" +table+"."+column+ ")" + " LIKE " + "LOWER('%"+relationOperator.getValue().toString()+"%')";
+        } else if (relationOperator.getOperatorType().equals(RelationOperatorType.In)) {
+           List values = relationOperator.getInValues().list().stream().map(value -> value instanceof String ? "'"+value+"'" : value.toString()).collect(Collectors.toList());
+            relationOperatorSQL = " " + table+"."+column + " IN " + "("+String.join(",", values)+")";
+        }
+        return relationOperatorSQL;
     }
 
     private String buildCondition(Condition condition, String table) {
@@ -78,11 +100,7 @@ public class QueryEngine extends Data {
             conditionSQL += this.buildWhereSQL(condition.getSubCondition().setTable(table));
             conditionSQL += ")";
         } else {
-            if (condition.getValue() instanceof String) {
-                conditionSQL += " " + condition.getOperator() + " " + table+"."+condition.getColumn() + " = " + "'" + condition.getValue().toString() + "'";
-            } else {
-                conditionSQL += " " + condition.getOperator() + " " + table+"."+condition.getColumn() + " = " + condition.getValue().toString();
-            }
+            conditionSQL += " " + condition.getOperator() + " " + this.buildRelationOperatorSQL(condition.getRelationOperator(), table, condition.getColumn());
         }
         return conditionSQL;
     }
