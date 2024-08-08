7b373210b3ab674ab4c04ecfddd97030b78d9adb
JailtonAraujo <jailtonraneimoney@gmail.com>
2024-08-08 10:24:01 -0300

    Tritao: Add Group object doc

--- netuno.tritao/src/main/java/org/netuno/tritao/query/Group.java
+++ netuno.tritao/src/main/java/org/netuno/tritao/query/Group.java
@@ -1,5 +1,22 @@
 package org.netuno.tritao.query;
 
+import org.netuno.library.doc.LanguageDoc;
+import org.netuno.library.doc.LibraryDoc;
+import org.netuno.library.doc.LibraryTranslationDoc;
+import org.netuno.library.doc.MethodDoc;
+import org.netuno.library.doc.MethodTranslationDoc;
+import org.netuno.library.doc.ParameterDoc;
+import org.netuno.library.doc.ParameterTranslationDoc;
+import org.netuno.library.doc.ReturnTranslationDoc;
+
+@LibraryDoc(translations = {
+    @LibraryTranslationDoc(
+            language = LanguageDoc.PT,
+            title = "Group",
+            introduction = "Definição do objeto Group para agrupamento de resultados com Query.",
+            howToUse = {}
+    )
+})
 public class Group {
     private String column;
 
@@ -7,10 +24,72 @@ public class Group {
         this.column = column;
     }
 
+    @MethodDoc(
+        translations = {
+            @MethodTranslationDoc(
+                language = LanguageDoc.PT,
+                description = "Retorna a coluna que será usada na efetuar o agrupamento.",
+                howToUse = {}
+            ),
+            @MethodTranslationDoc(
+                language = LanguageDoc.EN,
+                description = "Returns the column that will be used to perform the grouping.",
+                howToUse = {}
+            )
+        },
+        parameters = {},
+        returns = {
+            @ReturnTranslationDoc(
+                language = LanguageDoc.PT,
+                description = "Nome da coluna."
+            ),
+            @ReturnTranslationDoc(
+                language = LanguageDoc.EN,
+                description = "The name of the column."
+            )
+        }
+    )
     public String getColumn() {
         return column;
     }
 
+    @MethodDoc(
+        translations = {
+            @MethodTranslationDoc(
+                language = LanguageDoc.PT,
+                description = "Define a coluna que será usada na efetuar o agrupamento.",
+                howToUse = {}
+            ),
+            @MethodTranslationDoc(
+                language = LanguageDoc.EN,
+                description = "Defines the column that will be used to perform the grouping.",
+                howToUse = {}
+            )
+        },
+        parameters = {
+            @ParameterDoc(name = "column", translations = {
+                @ParameterTranslationDoc(
+                    language = LanguageDoc.PT,
+                    description = "Nome da coluna.",
+                    name = "coluna"
+                ),
+                @ParameterTranslationDoc(
+                    language = LanguageDoc.EN,
+                    description = "The name of the column."
+                )
+            })
+        },
+        returns = {
+            @ReturnTranslationDoc(
+                language = LanguageDoc.PT,
+                description = "Objeto Group atual."
+            ),
+            @ReturnTranslationDoc(
+                language = LanguageDoc.EN,
+                description = "Current Group object."
+            )
+        }
+    )
     public Group setColumn(String column) {
         this.column = column;
         return this;
