7826dc97ca7b1d6691c45146d5d353ad2a334a05
Eduardo Velasques <eduveks@gmail.com>
2024-11-04 19:37:42 +0000

    Proteu: Core object has dependencies updated to Jakarta Servlet.

--- netuno.proteu/src/main/java/org/netuno/proteu/Proteu.java
+++ netuno.proteu/src/main/java/org/netuno/proteu/Proteu.java
@@ -26,11 +26,11 @@ import org.netuno.psamata.io.InputStream;
 import org.netuno.psamata.io.MimeTypes;
 import org.netuno.psamata.io.SafePath;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.util.*;
