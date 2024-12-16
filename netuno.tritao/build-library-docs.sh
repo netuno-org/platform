fa3bbe1ef3dd0fd98b777834c2b366918a984aee
Eduardo Velasques <eduveks@gmail.com>
2024-12-16 19:42:34 +0000

    Tritao: The script of the documentation library generator has the paths updated.

--- netuno.tritao/build-library-docs.sh
+++ netuno.tritao/build-library-docs.sh
@@ -8,15 +8,15 @@ cd netuno.tritao
 
 mvn -Dtest=BuildLibraryTest test
 
-trash-put ../../doc/docs/library/objects
-trash-put ../../doc/docs/library/resources
+trash-put ../../doc/docs/library/objects/*.md
+trash-put ../../doc/docs/library/resources/*.md
 
-trash-put ../../doc/website/translated_docs/pt-PT/library/objects
-trash-put ../../doc/website/translated_docs/pt-PT/library/resources
+trash-put ../../doc/i18n/pt/docusaurus-plugin-content-docs/current/library/objects/*.md
+trash-put ../../doc/i18n/pt/docusaurus-plugin-content-docs/current/library/resources/*.md
 
-cp -r docs/EN/library/objects ../../doc/docs/library/objects
-cp -r docs/EN/library/resources ../../doc/docs/library/resources
+cp -r docs/EN/library/objects/*.md ../../doc/docs/library/objects/
+cp -r docs/EN/library/resources/*.md ../../doc/docs/library/resources/
 
-cp -r docs/PT/library/objects ../../doc/website/translated_docs/pt-PT/library/objects
-cp -r docs/PT/library/resources ../../doc/website/translated_docs/pt-PT/library/resources
+cp -r docs/PT/library/objects/*.md ../../doc/i18n/pt/docusaurus-plugin-content-docs/current/library/objects/
+cp -r docs/PT/library/resources/*.md ../../doc/i18n/pt/docusaurus-plugin-content-docs/current/library/resources/
 
