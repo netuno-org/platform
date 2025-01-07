c66ca7f5fe146d8253b79b6758a440fe8e7c2874
Eduardo Velasques <eduveks@gmail.com>
2025-01-07 15:01:06 +0000

    Tritao: Graal sandbox created to support JavaScript and Python.

---
+++ netuno.tritao/src/main/java/org/netuno/tritao/sandbox/GraalSandbox.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Netuno.org under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The Netuno.org licenses this file to You under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.netuno.tritao.sandbox;
+
+import org.netuno.psamata.Values;
+import org.netuno.psamata.script.GraalRunner;
+import org.netuno.tritao.config.Config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Graal Sandbox
+ * @author Eduardo Fonseca Velasques - @eduveks
+ */
+@ScriptSandbox(extensions = {"js", "py"})
+public class GraalSandbox implements Scriptable {
+    private SandboxManager manager;
+
+    private GraalRunner graalRunner = null;
+
+    public GraalSandbox(SandboxManager manager) {
+        this.manager = manager;
+
+        Map<String, String> options = new HashMap<>();
+        options.put("js.v8-compat", "true");
+        options.put("js.commonjs-require", "true");
+        options.put("js.commonjs-require-cwd", Config.getPathAppBaseServer(manager.getProteu()));
+
+        graalRunner = new GraalRunner(options, Config.getPermittedLanguages());
+    }
+
+    private String getGraalLanguage(String extension) {
+        if (!extension.equals("js") && !extension.equals("py")) {
+            throw new UnsupportedOperationException("The extension "+ extension +" is not supported.");
+        }
+        return switch (extension) {
+            case "py" -> "python";
+            default -> extension;
+        };
+    }
+
+    @Override
+    public void resetContext() {
+        graalRunner.newContext();
+    }
+
+    @Override
+    public void run(ScriptSourceCode script, Values bindings) throws Exception {
+        String lang = getGraalLanguage(script.extension());
+        bindings.forEach((k, v) -> graalRunner.set(lang, k.toString(), v));
+        graalRunner.eval(lang, script.content());
+    }
+
+    @Override
+    public void stop() throws Exception {
+        graalRunner.closeContext();
+    }
+
+    @Override
+    public void close() throws Exception {
+        graalRunner.close();
+        graalRunner = null;
+    }
+}
