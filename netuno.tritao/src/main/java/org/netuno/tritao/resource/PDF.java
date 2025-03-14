ae6fd1766986652abd5fe36737f8fb9a69ba9d1e
Eduardo Velasques <eduveks@gmail.com>
2025-03-14 09:19:55 +0000

    Tritao: PDF resource has others to image methods implementations.

--- netuno.tritao/src/main/java/org/netuno/tritao/resource/PDF.java
+++ netuno.tritao/src/main/java/org/netuno/tritao/resource/PDF.java
@@ -72,6 +72,7 @@ import org.netuno.tritao.resource.util.FileSystemPath;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.FileInputStream;
 import java.io.StringWriter;
@@ -4652,6 +4653,66 @@ public class PDF extends ResourceBase {
         }
     }
 
+    public void toImage(Storage source, Storage destinationPath, String filePrefixName, String fileExtension) throws IOException {
+        toImage(source, -1, destinationPath, filePrefixName, fileExtension);
+    }
+
+    public void toImage(Storage source, Storage destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(source, -1, -1, destinationPath, filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(Storage source, int pageNumber, Storage destinationPath, String filePrefixName, String fileExtension) throws IOException {
+        toImage(source, pageNumber, pageNumber, destinationPath, filePrefixName, fileExtension, 300);
+    }
+
+    public void toImage(Storage source, int pageNumber, Storage destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(source, pageNumber, pageNumber, destinationPath, filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(Storage source, int startPage, int endPage, Storage destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(source.file(), startPage, endPage, destinationPath.folder(), filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(Storage source, File destinationPath, String filePrefixName, String fileExtension) throws IOException {
+        toImage(source, -1, destinationPath, filePrefixName, fileExtension);
+    }
+
+    public void toImage(Storage source, File destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(source, -1, -1, destinationPath, filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(Storage source, int pageNumber, File destinationPath, String filePrefixName, String fileExtension) throws IOException {
+        toImage(source, pageNumber, pageNumber, destinationPath, filePrefixName, fileExtension, 300);
+    }
+
+    public void toImage(Storage source, int pageNumber, File destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(source, pageNumber, pageNumber, destinationPath, filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(Storage source, int startPage, int endPage, File destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(source.file(), startPage, endPage, destinationPath, filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(File source, Storage destinationPath, String filePrefixName, String fileExtension) throws IOException {
+        toImage(source, -1, destinationPath, filePrefixName, fileExtension);
+    }
+
+    public void toImage(File source, Storage destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(source, -1, -1, destinationPath, filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(File source, int pageNumber, Storage destinationPath, String filePrefixName, String fileExtension) throws IOException {
+        toImage(source, pageNumber, pageNumber, destinationPath, filePrefixName, fileExtension, 300);
+    }
+
+    public void toImage(File source, int pageNumber, Storage destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(source, pageNumber, pageNumber, destinationPath, filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(File source, int startPage, int endPage, Storage destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(source, startPage, endPage, destinationPath.folder(), filePrefixName, fileExtension, dpi);
+    }
+
     public void toImage(File source, File destinationPath, String filePrefixName, String fileExtension) throws IOException {
         toImage(source, -1, destinationPath, filePrefixName, fileExtension);
     }
@@ -4672,11 +4733,90 @@ public class PDF extends ResourceBase {
         try (PDDocument document = Loader.loadPDF(new RandomAccessReadBufferedFile(source.getFullPath()))) {
             PDFRenderer pdfRenderer = new PDFRenderer(document);
             int numberOfPages = document.getNumberOfPages();
-            for (int i = (startPage >= 0 ? startPage : 0); i < (endPage >= 0 ? endPage + 1 : numberOfPages); ++i) {
+            for (int i = (startPage >= 0 ? startPage : 0); i < (endPage >= 0 ? endPage : numberOfPages); ++i) {
                 java.io.File outFile = new java.io.File(destinationPath.fullPath(), filePrefixName + "-" + (i + 1) + "." + fileExtension);
                 BufferedImage bufImage = pdfRenderer.renderImageWithDPI(i, dpi, fileExtension.equalsIgnoreCase("png") ? ImageType.ARGB : ImageType.RGB);
                 ImageIO.write(bufImage, fileExtension, outFile);
             }
+        }
+    }
+
+    public void toImage(java.io.InputStream in, Storage destinationPath, String filePrefixName, String fileExtension) throws IOException {
+        toImage(in, -1, destinationPath, filePrefixName, fileExtension);
+    }
+
+    public void toImage(java.io.InputStream in, Storage destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(in, -1, -1, destinationPath, filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(java.io.InputStream in, int pageNumber, Storage destinationPath, String filePrefixName, String fileExtension) throws IOException {
+        toImage(in, pageNumber, pageNumber, destinationPath, filePrefixName, fileExtension, 300);
+    }
+
+    public void toImage(java.io.InputStream in, int pageNumber, Storage destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(in, pageNumber, pageNumber, destinationPath, filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(java.io.InputStream in, int startPage, int endPage, Storage destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(in, startPage, endPage, destinationPath.folder(), filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(java.io.InputStream in, File destinationPath, String filePrefixName, String fileExtension) throws IOException {
+        toImage(in, -1, destinationPath, filePrefixName, fileExtension);
+    }
+
+    public void toImage(java.io.InputStream in, File destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(in, -1, -1, destinationPath, filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(java.io.InputStream in, int pageNumber, File destinationPath, String filePrefixName, String fileExtension) throws IOException {
+        toImage(in, pageNumber, pageNumber, destinationPath, filePrefixName, fileExtension, 300);
+    }
+
+    public void toImage(java.io.InputStream in, int pageNumber, File destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        toImage(in, pageNumber, pageNumber, destinationPath, filePrefixName, fileExtension, dpi);
+    }
+
+    public void toImage(java.io.InputStream in, int startPage, int endPage, File destinationPath, String filePrefixName, String fileExtension, int dpi) throws IOException {
+        try (PDDocument document = Loader.loadPDF(RandomAccessReadBuffer.createBufferFromStream(in))) {
+            PDFRenderer pdfRenderer = new PDFRenderer(document);
+            int numberOfPages = document.getNumberOfPages();
+            for (int i = (startPage >= 0 ? startPage : 0); i < (endPage >= 0 ? endPage : numberOfPages); ++i) {
+                java.io.File outFile = new java.io.File(destinationPath.fullPath(), filePrefixName + "-" + (i + 1) + "." + fileExtension);
+                BufferedImage bufImage = pdfRenderer.renderImageWithDPI(i, dpi, fileExtension.equalsIgnoreCase("png") ? ImageType.ARGB : ImageType.RGB);
+                ImageIO.write(bufImage, fileExtension, outFile);
+            }
+        }
+    }
+
+    public byte[][] toImage(java.io.InputStream in, String fileExtension) throws IOException {
+        return toImage(in, -1, fileExtension);
+    }
+
+    public byte[][] toImage(java.io.InputStream in, String fileExtension, int dpi) throws IOException {
+        return toImage(in, -1, -1, fileExtension, dpi);
+    }
+
+    public byte[][] toImage(java.io.InputStream in, int pageNumber, String fileExtension) throws IOException {
+        return toImage(in, pageNumber, pageNumber, fileExtension, 300);
+    }
+
+    public byte[][] toImage(java.io.InputStream in, int pageNumber, String fileExtension, int dpi) throws IOException {
+        return toImage(in, pageNumber, pageNumber, fileExtension, dpi);
+    }
+
+    public byte[][] toImage(java.io.InputStream in, int startPage, int endPage, String fileExtension, int dpi) throws IOException {
+        try (PDDocument document = Loader.loadPDF(RandomAccessReadBuffer.createBufferFromStream(in))) {
+            PDFRenderer pdfRenderer = new PDFRenderer(document);
+            int numberOfPages = document.getNumberOfPages();
+            byte[][] images = new byte[endPage - startPage][];
+            for (int i = (startPage >= 0 ? startPage : 0); i < (endPage >= 0 ? endPage : numberOfPages); ++i) {
+                BufferedImage bufImage = pdfRenderer.renderImageWithDPI(i, dpi, fileExtension.equalsIgnoreCase("png") ? ImageType.ARGB : ImageType.RGB);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ImageIO.write(bufImage, fileExtension, baos);
+                images[startPage + i - endPage] = baos.toByteArray();
+            }
+            return images;
         }
     }
 
