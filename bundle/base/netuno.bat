a638b55514c0524cd0a7137f462eafba00311063
Eduardo Velasques <eduveks@gmail.com>
2025-04-08 16:15:35 +0100

    Bundle: Netuno script upgrade with Java parameters to avoid warnings in the terminal.

--- bundle/base/netuno.bat
+++ bundle/base/netuno.bat
@@ -1,2 +1,2 @@
 
-graalvm\bin\java -Dgraal.LogFile=logs\graal.log -Dfile.encoding="UTF-8" -jar netuno.jar %*
+graalvm\bin\java --enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow -Djdk.graal.LogFile=logs\graal.log -Dfile.encoding="UTF-8" -jar netuno.jar %*
