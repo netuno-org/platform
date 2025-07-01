3268b17e2e28c3d8ec825aea2f7dea39fe886196
Eduardo Velasques <eduveks@gmail.com>
2025-07-01 20:27:01 +0100

    Psamata: Process tests with delay and discard.

--- netuno.psamata/src/test/java/org/netuno/psamata/os/ProcessTest.java
+++ netuno.psamata/src/test/java/org/netuno/psamata/os/ProcessTest.java
@@ -72,4 +72,36 @@ public class ProcessTest {
         assertEquals("1", result.output().trim(), "Output");
         checkThreads();
     }
+
+    @Test
+    public void delay() throws IOException, InterruptedException {
+        Process process = new Process();
+        Process.Result result = process.execute("sleep 1 && echo 1 && sleep 1");
+        assertEquals(0, result.exitCode(), "Exit Code");
+        assertEquals("1", result.output().toString().trim(), "Output");
+        checkThreads();
+    }
+
+    @Test
+    public void delayOutput() throws IOException, InterruptedException {
+        Process process = new Process();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        process.outputStream(baos);
+        Process.Result result = process.execute("sleep 1 && echo 1 && sleep 1");
+        assertEquals(0, result.exitCode(), "Exit Code");
+        assertEquals("1", baos.toString().trim(), "Output");
+        checkThreads();
+    }
+
+    @Test
+    public void discardOutput() throws IOException, InterruptedException {
+        Process process = new Process();
+        process.readOutput(false);
+        process.readErrorOutput(false);
+        Process.Result result = process.execute("echo 1");
+        assertEquals(0, result.exitCode(), "Exit Code");
+        assertEquals(null, result.output(), "Output");
+        assertEquals(null, result.outputError(), "Output Error");
+        checkThreads();
+    }
 }
