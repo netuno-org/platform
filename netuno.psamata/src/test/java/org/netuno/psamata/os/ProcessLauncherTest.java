acada5d9554069bcea4bfecca232daef5d8acc31
Eduardo Velasques <eduveks@gmail.com>
2025-07-02 12:38:09 +0100

    Psamata: Process Test renamed, and parallel test added.

--- netuno.psamata/src/test/java/org/netuno/psamata/os/ProcessLauncherTest.java
+++ netuno.psamata/src/test/java/org/netuno/psamata/os/ProcessLauncherTest.java
@@ -4,39 +4,40 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ProcessTest {
+public class ProcessLauncherTest {
 
     private void checkThreads() {
         for (Thread thread : Thread.getAllStackTraces().keySet()) {
-            assertFalse(thread.getName().startsWith("Netuno Psamata:"), "");
+            assertFalse(thread.getName().startsWith("Netuno Psamata:"), "Thread still alive: "+ thread.getName());
         }
     }
 
     @Test
     public void execute() throws IOException, InterruptedException {
-        Process process = new Process();
-        Process.Result result = process.execute("echo", "1");
+        ProcessLauncher processLauncher = new ProcessLauncher();
+        ProcessLauncher.Result result = processLauncher.execute("echo", "1");
         assertEquals(0, result.exitCode, "Exit Code");
         assertEquals("1", result.output.trim(), "Output");
     }
 
     @Test
     public void executeOutput() throws IOException, InterruptedException {
-        Process process = new Process();
+        ProcessLauncher processLauncher = new ProcessLauncher();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        process.outputStream(baos);
-        Process.Result result = process.execute("echo", "1");
+        processLauncher.outputStream(baos);
+        ProcessLauncher.Result result = processLauncher.execute("echo", "1");
         assertEquals(0, result.exitCode, "Exit Code");
         assertEquals("1", baos.toString().trim(), "Output");
     }
 
     @Test
     public void async() throws IOException, InterruptedException {
-        Process process = new Process();
-        Process.Result result = process.executeAsync("echo", "1");
+        ProcessLauncher processLauncher = new ProcessLauncher();
+        ProcessLauncher.Result result = processLauncher.executeAsync("echo", "1");
         assertEquals(0, result.exitCode, "Exit Code");
         assertEquals("1", result.output().trim(), "Output");
         checkThreads();
@@ -44,10 +45,10 @@ public class ProcessTest {
 
     @Test
     public void asyncOutput() throws IOException, InterruptedException {
-        Process process = new Process();
+        ProcessLauncher processLauncher = new ProcessLauncher();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        process.outputStream(baos);
-        Process.Result result = process.executeAsync("echo", "1");
+        processLauncher.outputStream(baos);
+        ProcessLauncher.Result result = processLauncher.executeAsync("echo", "1");
         assertEquals(0, result.exitCode(), "Exit Code");
         assertEquals("1", baos.toString().trim(), "Output");
         checkThreads();
@@ -55,9 +56,9 @@ public class ProcessTest {
 
     @Test
     public void timeLimit() throws IOException, InterruptedException {
-        Process process = new Process();
-        process.timeLimit(1000);
-        Process.Result result = process.execute("echo", "1", "&&", "sleep", "2", "&&", "echo", "2");
+        ProcessLauncher processLauncher = new ProcessLauncher();
+        processLauncher.timeLimit(1000);
+        ProcessLauncher.Result result = processLauncher.execute("echo", "1", "&&", "sleep", "2", "&&", "echo", "2");
         assertEquals(143, result.exitCode(), "Exit Code");
         assertEquals("1", result.output().toString().trim(), "Output");
         checkThreads();
@@ -65,9 +66,9 @@ public class ProcessTest {
 
     @Test
     public void timeLimitAsync() throws IOException, InterruptedException {
-        Process process = new Process();
-        process.timeLimit(1000);
-        Process.Result result = process.executeAsync("echo", "1", "&&", "sleep", "2", "&&", "echo", "2");
+        ProcessLauncher processLauncher = new ProcessLauncher();
+        processLauncher.timeLimit(1000);
+        ProcessLauncher.Result result = processLauncher.executeAsync("echo", "1", "&&", "sleep", "2", "&&", "echo", "2");
         assertEquals(143, result.exitCode(), "Exit Code");
         assertEquals("1", result.output().trim(), "Output");
         checkThreads();
@@ -75,8 +76,8 @@ public class ProcessTest {
 
     @Test
     public void delay() throws IOException, InterruptedException {
-        Process process = new Process();
-        Process.Result result = process.execute("sleep 1 && echo 1 && sleep 1");
+        ProcessLauncher processLauncher = new ProcessLauncher();
+        ProcessLauncher.Result result = processLauncher.execute("sleep 1 && echo 1 && sleep 1");
         assertEquals(0, result.exitCode(), "Exit Code");
         assertEquals("1", result.output().toString().trim(), "Output");
         checkThreads();
@@ -84,10 +85,10 @@ public class ProcessTest {
 
     @Test
     public void delayOutput() throws IOException, InterruptedException {
-        Process process = new Process();
+        ProcessLauncher processLauncher = new ProcessLauncher();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        process.outputStream(baos);
-        Process.Result result = process.execute("sleep 1 && echo 1 && sleep 1");
+        processLauncher.outputStream(baos);
+        ProcessLauncher.Result result = processLauncher.execute("sleep 1 && echo 1 && sleep 1");
         assertEquals(0, result.exitCode(), "Exit Code");
         assertEquals("1", baos.toString().trim(), "Output");
         checkThreads();
@@ -95,13 +96,29 @@ public class ProcessTest {
 
     @Test
     public void discardOutput() throws IOException, InterruptedException {
-        Process process = new Process();
-        process.readOutput(false);
-        process.readErrorOutput(false);
-        Process.Result result = process.execute("echo 1");
+        ProcessLauncher processLauncher = new ProcessLauncher();
+        processLauncher.readOutput(false);
+        processLauncher.readErrorOutput(false);
+        ProcessLauncher.Result result = processLauncher.execute("echo 1");
         assertEquals(0, result.exitCode(), "Exit Code");
         assertEquals(null, result.output(), "Output");
         assertEquals(null, result.outputError(), "Output Error");
+        checkThreads();
+    }
+
+    @Test
+    public void parallel() throws IOException, InterruptedException {
+        AtomicInteger counter = new AtomicInteger();
+        ProcessLauncher processLauncher = new ProcessLauncher();
+        processLauncher.onParallel((t)-> {
+            while (t.isRunning()) {
+                counter.set(counter.get() + 1);
+                t.pause(250);
+            }
+        });
+        ProcessLauncher.Result result = processLauncher.execute("sleep 1");
+        assertEquals(0, result.exitCode(), "Exit Code");
+        assertTrue(counter.get() >= 4, "Counter");
         checkThreads();
     }
 }
