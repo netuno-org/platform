b9e85956e9fbfeedf08199d2174b6447907c78ae
Eduardo Velasques <eduveks@gmail.com>
2025-07-11 13:55:15 +0100

    Bundle: Demo app UI uses the AntD Table for data visualization.

--- bundle/base/apps/demo/ui/src/components/TableList/index.jsx
+++ bundle/base/apps/demo/ui/src/components/TableList/index.jsx
@@ -2,31 +2,25 @@ import React from 'react';
 
 import { FormattedMessage } from 'react-intl';
 
+import {Table} from 'antd';
+
 import './index.less';
 
 const messages = 'dashboardcontainer.datavisualization.table';
 
 function TableList({data = []}) {
+  const columns = [
+      {
+        title: <FormattedMessage id={`${messages}.workers`} />,
+        dataIndex: 'name',
+      },
+      {
+        title: <FormattedMessage id={`${messages}.total`} />,
+        dataIndex: 'total',
+      }
+  ];
   return (
-    <table className="table table-list">
-      <thead>
-        <tr>
-          <th><FormattedMessage id={`${messages}.workers`} /></th>
-          <th><FormattedMessage id={`${messages}.total`} /></th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          data.map(
-            (record, i) =>
-            <tr key={i}>
-              <td>{record.name}</td>
-              <td>{record.total}</td>
-            </tr>
-          )
-        }
-      </tbody>
-    </table>
+    <Table columns={columns} dataSource={data} pagination={false} bordered={true} />
   );
 }
 
