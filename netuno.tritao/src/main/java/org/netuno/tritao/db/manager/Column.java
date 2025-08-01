a845e3f360818b713f4c28fb97ff6f00a1dcf193
Eduardo Velasques <eduveks@gmail.com>
2025-08-01 17:48:35 +0100

    Tritao: Column manager change type or default value issue solved for the H2 Database.

--- netuno.tritao/src/main/java/org/netuno/tritao/db/manager/Column.java
+++ netuno.tritao/src/main/java/org/netuno/tritao/db/manager/Column.java
@@ -436,7 +436,8 @@ public class Column extends ManagerBase {
                     getExecutor().execute("alter table " + getBuilder().escape(DB.sqlInjectionRawName(table)) + " alter column " + getBuilder().escape(DB.sqlInjectionRawName(getName())) + " set " + toDefaultDefinition() + ";");
                 }
             } else {
-                getExecutor().execute("alter table " + getBuilder().escape(DB.sqlInjectionRawName(table)) + " alter column " + getBuilder().escape(DB.sqlInjectionRawName(getName())) + " type " + toTypeDefinition() + " " + toDefaultDefinition() + ";");
+                getExecutor().execute("alter table " + getBuilder().escape(DB.sqlInjectionRawName(table)) + " alter column " + getBuilder().escape(DB.sqlInjectionRawName(getName())) + " type " + toTypeDefinition() + ";");
+                getExecutor().execute("alter table " + getBuilder().escape(DB.sqlInjectionRawName(table)) + " alter column " + getBuilder().escape(DB.sqlInjectionRawName(getName())) + " set default " + toDefaultDefinition() + ";");
             }
         } catch (Exception e) {
             throw new DBError(e).setLogFatal("Changing column type on "+ table +"."+ getName());
