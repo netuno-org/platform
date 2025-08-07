a41ccd45e8bf63b00cb9fc6857ba89bd7d14ab88
Eduardo Velasques <eduveks@gmail.com>
2025-08-07 18:59:53 +0100

    CLI: GraalVM version up to date.

--- netuno.cli/src/main/java/org/netuno/cli/setup/Constants.java
+++ netuno.cli/src/main/java/org/netuno/cli/setup/Constants.java
@@ -23,7 +23,7 @@ package org.netuno.cli.setup;
  * @author Eduardo Fonseca Velasques - @eduveks
  */
 public class Constants {
-    public static String GRAALVM_VERSION = "24.0.1";
+    public static String GRAALVM_VERSION = "24.0.2";
 
     public static String GRAALVM_FOLDER = "graalvm";
 
