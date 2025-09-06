bbda546e4fb06c36a71466afc653875a7fba6412
Eduardo Velasques <eduveks@gmail.com>
2025-09-06 18:22:06 +0100

    Tritao: UID component with mandatory value validation implemented.

--- netuno.tritao/src/main/java/org/netuno/tritao/com/UID.java
+++ netuno.tritao/src/main/java/org/netuno/tritao/com/UID.java
@@ -75,6 +75,14 @@ public class UID extends ComponentBase {
     public String getHtmlValue() {
         return value;
     }
+
+    @Override
+    public boolean isMandatoryValueOk() {
+        if (isModeSave() && getDesignData().getBoolean("notnull")) {
+            return value != null && !value.isEmpty();
+        }
+        return true;
+    }
     
     public Component getInstance(Proteu proteu, Hili hili) {
         return new UID(proteu, hili, this);
