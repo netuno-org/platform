31134de161a5eee06442b8c03ec23f074b3d8c3d
Eduardo Velasques <eduveks@gmail.com>
2025-10-02 22:16:20 +0100

    Tritao: Authentication Google provider was cleaned up.

--- netuno.tritao/src/main/java/org/netuno/tritao/auth/providers/entities/Google.java
+++ netuno.tritao/src/main/java/org/netuno/tritao/auth/providers/entities/Google.java
@@ -15,16 +15,15 @@
  * limitations under the License.
  */
 
-/**
- * Google Authentication
- * @author Marcel Becheanu - @marcelgbecheanu
- */
-
 package org.netuno.tritao.auth.providers.entities;
 
 import org.netuno.psamata.Values;
 import org.netuno.tritao.auth.providers.Callback;
 
+/**
+ * Google Authentication
+ * @author Marcel Becheanu - @marcelgbecheanu
+ */
 public class Google {
     private String id = null;
     private String secret = null;
@@ -57,57 +56,4 @@ public class Google {
         details.set("avatar", details.getString("picture"));
         return details;
     }
-
-    /*public JSONObject getAccessTokens(String code){
-        try{
-            URL url = new URL("https://accounts.google.com/o/oauth2/token" );
-            URLConnection con = url.openConnection();
-            HttpURLConnection http = (HttpURLConnection)con;
-            http.setRequestMethod("POST");
-            http.setDoOutput(true);
-            Map<String,String> arguments = new HashMap<>();
-            arguments.put("code", code);
-            arguments.put("client_id", id);
-            arguments.put("client_secret", secret);
-            arguments.put("redirect_uri", callbackUrl);
-            arguments.put("grant_type", "authorization_code");
-            StringJoiner sj = new StringJoiner("&");
-            for(Map.Entry<String,String> entry : arguments.entrySet())
-                sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "="
-                        + URLEncoder.encode(entry.getValue(), "UTF-8"));
-            byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
-            int length = out.length;
-            http.setFixedLengthStreamingMode(length);
-            http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-            http.connect();
-            try(OutputStream os = http.getOutputStream()) {
-                os.write(out);
-            }
-            String response;
-            try(InputStream inputStream = http.getInputStream()) {
-                response = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-            }
-            http.disconnect();
-            JSONObject data = new JSONObject(response);
-            return data;
-        }catch (Exception e){
-            return null;
-        }
-    }*/
-   /* public JSONObject getUserDetails(JSONObject data){
-        try{
-            URL url = new URL("https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + data.get("access_token") );
-            URLConnection con = url.openConnection();
-            HttpURLConnection http = (HttpURLConnection)con;
-            http.setRequestMethod("GET");
-            http.setDoOutput(true);
-            String response;
-            try(InputStream inputStream = http.getInputStream()) {
-                response = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-            }
-            return new JSONObject(response);
-        }catch (Exception e){
-            return null;
-        }
-    }*/
 }
