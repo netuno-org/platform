95bea0be4102eedd03e40ca7f341701c74248802
Eduardo Velasques <eduveks@gmail.com>
2025-10-02 22:12:16 +0100

    Tritao: Authentication Providers has the Requests accepting JSON.

--- netuno.tritao/src/main/java/org/netuno/tritao/auth/providers/entities/Requests.java
+++ netuno.tritao/src/main/java/org/netuno/tritao/auth/providers/entities/Requests.java
@@ -1,10 +1,26 @@
+/*
+ * Licensed to the Netuno.org under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The Netuno.org licenses this file to You under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.netuno.tritao.auth.providers.entities;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.netuno.psamata.Values;
 
-import javax.swing.plaf.basic.BasicOptionPaneUI;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -16,6 +32,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
 
+/**
+ * Requests for the Authentication Providers
+ * @author Marcel Becheanu - @marcelgbecheanu
+ * @author Eduardo Fonseca Velasques - @eduveks
+ */
 public class Requests {
     public static Values makeGet(String url, Values params) throws Exception{
         return makeGet(url, params, null);
@@ -43,6 +64,7 @@ public class Requests {
         if (authorization != null) {
             http.setRequestProperty("Authorization", authorization);
         }
+        http.setRequestProperty("Accept", "application/json");
         http.setRequestMethod("GET");
         http.setDoOutput(true);
         String response;
@@ -55,6 +77,7 @@ public class Requests {
         URL urlCache = new URL(url);
         URLConnection con = urlCache.openConnection();
         HttpURLConnection http = (HttpURLConnection)con;
+        http.setRequestProperty("Accept", "application/json");
         http.setRequestMethod("POST");
         http.setDoOutput(true);
         Map<String,String> arguments = new HashMap<>();
