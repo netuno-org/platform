4e2e5baec8d68e7e11c1194c4e703c82d99614db
Eduardo Velasques <eduveks@gmail.com>
2025-10-03 19:15:11 +0100

    Tritao: Microsoft Authentication Provider refactored.

--- netuno.tritao/src/main/java/org/netuno/tritao/auth/providers/integration/Microsoft.java
+++ netuno.tritao/src/main/java/org/netuno/tritao/auth/providers/integration/Microsoft.java
@@ -15,26 +15,36 @@
  * limitations under the License.
  */
 
-package org.netuno.tritao.auth.providers.entities;
+package org.netuno.tritao.auth.providers.integration;
 
 import org.netuno.psamata.Values;
 import org.netuno.tritao.auth.providers.Callback;
 
+import java.util.UUID;
+
 /**
- * Microsoft Authentication
+ * Microsoft Authentication Provider
  * @author Eduardo Fonseca Velasques - @eduveks
  */
-public class Microsoft {
-    private String tenant = null;
-    private String id = null;
-    private String secret = null;
-    private Values callbacks = null;
-    public Microsoft(String tenant, String id, String secret, Values callbacks) {
-        this.tenant = tenant;
-        this.id = id;
-        this.secret = secret;
-        this.callbacks = callbacks;
+public class Microsoft implements Provider {
+    private static final String CODE = "microsoft";
+    private static final String STATE = UUID.randomUUID().toString();
+    private final String tenant;
+    private final String id;
+    private final String secret;
+    private final Values callbacks;
+
+    public Microsoft(Values settings) {
+        this.tenant = settings.getString("tenant");
+        this.id = settings.getString("id");
+        this.secret = settings.getString("secret");
+        this.callbacks = settings.getValues("callbacks");
     }
+
+    public String getCode() {
+        return CODE;
+    }
+
     public String getUrlAuthenticator(Callback callback) {
             return "https://login.microsoftonline.com/"
                     + (tenant == null || tenant.isEmpty() ? "common" : tenant)
@@ -42,7 +52,8 @@ public class Microsoft {
                     + "?response_type=code"
                     + "&redirect_uri=" + callbacks.getString(callback.toString())
                     + "&scope=User.Read,ProfilePhoto.Read.All"
-                    + "&client_id=" + id;
+                    + "&client_id=" + id
+                    + "&state=" + STATE;
     }
 
     public Values getAccessTokens(Callback callback, String code) throws Exception {
@@ -63,11 +74,16 @@ public class Microsoft {
         Values params = new Values();
         Values details = Requests.makeGet(url, params, "Bearer "+ data.getString("access_token"));
 
+        details.put("id", details.getString("id"));
+        details.put("name", details.getString("displayName"));
+        details.put("username", details.getString("userPrincipalName"));
+
         // https://graph.microsoft.com/v1.0/me/photos/240x240/$value
         // Header Authorization Token
         // Content-Type JPG
 
         details.set("avatar", details.getString("picture"));
+
         return details;
     }
 }
