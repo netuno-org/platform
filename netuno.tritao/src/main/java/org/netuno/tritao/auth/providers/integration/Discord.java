b257a699a7103bdfe642bf7c067934361bd1fb97
Eduardo Velasques <eduveks@gmail.com>
2025-10-03 19:13:58 +0100

    Tritao: Discord Authentication Provider refactored.

--- netuno.tritao/src/main/java/org/netuno/tritao/auth/providers/integration/Discord.java
+++ netuno.tritao/src/main/java/org/netuno/tritao/auth/providers/integration/Discord.java
@@ -1,17 +1,43 @@
-package org.netuno.tritao.auth.providers.entities;
+/*
+ * Licensed to the Netuno.org under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The Netuno.org licenses this file to You under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.netuno.tritao.auth.providers.integration;
 
 import org.netuno.psamata.Values;
 import org.netuno.tritao.auth.providers.Callback;
 
-public class Discord {
-    private String id;
-    private String secret;
-    private Values callbacks;
+/**
+ * Discord Authentication Provider
+ * @author Eduardo Fonseca Velasques - @eduveks
+ */
+public class Discord implements Provider {
+    private static final String CODE = "discord";
+    private final String id;
+    private final String secret;
+    private final Values callbacks;
 
-    public Discord(String id, String secret, Values callbacks) {
-        this.id = id;
-        this.secret = secret;
-        this.callbacks = callbacks;
+    public Discord(Values settings) {
+        this.id = settings.getString("id");
+        this.secret = settings.getString("secret");
+        this.callbacks = settings.getValues("callbacks");
+    }
+
+    public String getCode() {
+        return CODE;
     }
 
     public String getUrlAuthenticator(Callback callback) {
