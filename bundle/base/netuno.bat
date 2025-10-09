3be216803323e19f1c44bdd0692087363719eb2d
Eduardo Velasques <eduveks@gmail.com>
2025-10-09 11:13:15 +0100

    Bundle: Launch scripts supports core folder.

--- bundle/base/netuno.bat
+++ bundle/base/netuno.bat
@@ -1,2 +1,2 @@
 
-graalvm\bin\java --enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow -Djdk.graal.LogFile=logs\graal.log -Dfile.encoding="UTF-8" -jar netuno.jar %*
+core\graalvm\bin\java --enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow -Djdk.graal.LogFile=logs\graal.log -Dfile.encoding="UTF-8" -jar netuno.jar %*
--- bundle/base/netuno.sh
+++ bundle/base/netuno.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-graalvm/bin/java --enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow -Djdk.graal.LogFile=logs/graal.log -Dfile.encoding=UTF-8 -jar netuno.jar $@
+core/graalvm/bin/java --enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow -Djdk.graal.LogFile=logs/graal.log -Dfile.encoding=UTF-8 -jar netuno.jar $@
