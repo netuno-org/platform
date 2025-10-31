e3414167981f65f3ef1938d39e34fc83b9478cb9
Eduardo Velasques <eduveks@gmail.com>
2025-10-31 10:46:34 +0000

    MVN install removed.

--- mvn-install.ps1
+++
@@ -1,4 +0,0 @@
-mvn clean
-mvn compile
-mvn -"Dmaven.test.skip=true" package
-mvn -"Dmaven.test.skip=true" install
--- mvn-install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mvn clean && mvn compile && mvn -Dmaven.test.skip=true package && mvn -Dmaven.test.skip=true install
-
