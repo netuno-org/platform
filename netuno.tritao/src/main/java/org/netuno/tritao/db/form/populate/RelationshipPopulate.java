40e655cb72af3fc8c250ab46f080275ade015468
JailtonAraujo <jailtonraneimoney@gmail.com>
2026-01-24 02:41:11 -0300

    tritao: db.form - adding link and reference in relationshipPopulate

--- netuno.tritao/src/main/java/org/netuno/tritao/db/form/populate/RelationshipPopulate.java
+++ netuno.tritao/src/main/java/org/netuno/tritao/db/form/populate/RelationshipPopulate.java
@@ -5,6 +5,8 @@ import org.netuno.tritao.db.form.join.RelationshipType;
 public class RelationshipPopulate {
     private String form;
     private String columnLink;
+    private String formLink;
+    private String columnReference;
     private RelationshipType relationshipType = RelationshipType.ManyToOne;
 
     public RelationshipPopulate() {}
@@ -15,12 +17,38 @@ public class RelationshipPopulate {
         this.relationshipType = relationshipType;
     }
 
+    public RelationshipPopulate(String form, String columnLink, String formLink, String columnReference, RelationshipType relationshipType) {
+        this.form = form;
+        this.columnLink = columnLink;
+        this.formLink = formLink;
+        this.columnReference = columnReference;
+        this.relationshipType = relationshipType;
+    }
+
     public String getForm() {
         return form;
     }
 
     public RelationshipPopulate setForm(String form) {
         this.form = form;
+        return this;
+    }
+
+    public String getFormLink() {
+        return formLink;
+    }
+
+    public RelationshipPopulate setFormLink(String formLink) {
+        this.formLink = formLink;
+        return this;
+    }
+
+    public String getColumnReference() {
+        return columnReference;
+    }
+
+    public RelationshipPopulate setColumnReference(String columnReference) {
+        this.columnReference = columnReference;
         return this;
     }
 
