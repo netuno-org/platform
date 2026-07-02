a00931fa4e397e05bcfaf45af789513bc7f7f0d5
Eduardo Velasques <eduveks@gmail.com>
2026-07-02 16:36:53 +0100

    Tritao: DB Table Operations supports table update with custom data.

--- netuno.tritao/src/main/java/org/netuno/tritao/db/builder/TableOperations.java
+++ netuno.tritao/src/main/java/org/netuno/tritao/db/builder/TableOperations.java
@@ -116,16 +116,20 @@ public interface TableOperations extends BuilderBase, TableSelect, TableDesignSe
     }
 
     default boolean updateTable() {
+        return updateTable(getProteu().getRequestAll());
+    }
+
+    default boolean updateTable(Values data) {
         try {
-            String newName = getProteu().getRequestAll().getString("name");
+            String newName = data.getString("name");
             List<Values> tablesNew = selectTable("", newName, "");
-            List<Values> tablesOld = selectTable(getProteu().getRequestPost().getString("id"), "", "");
+            List<Values> tablesOld = selectTable(data.getString("id"), "", "");
             if (tablesOld.size() != 1 || (tablesNew.size() > 0
-                    && tablesNew.get(0).getInt("id") != getProteu().getRequestAll().getInt("id"))) {
+                    && tablesNew.get(0).getInt("id") != data.getInt("id"))) {
                 return false;
             }
             String oldName = tablesOld.get(0).getString("name");
-            if (!getProteu().getRequestAll().getBoolean("report") && !oldName.equals(newName)) {
+            if (!data.getBoolean("report") && !oldName.equals(newName)) {
                 new Sequence(this).rename(oldName + "_id", newName + "_id");
                 new Table(this).rename(oldName, newName);
                 List<Values> tableDesign = selectTableDesign("");
@@ -153,52 +157,52 @@ public interface TableOperations extends BuilderBase, TableSelect, TableDesignSe
                 }
             }
             List<Values> rsParentTable = null;
-            if (getProteu().getRequestAll().getInt("parent_id") > 0) {
+            if (data.getInt("parent_id") > 0) {
                 rsParentTable = Config.getDBBuilder(getProteu())
-                        .selectTable(getProteu().getRequestAll().getString("parent_id"));
-            } else if (!getProteu().getRequestAll().getString("parent_uid").isEmpty()) {
+                        .selectTable(data.getString("parent_id"));
+            } else if (!data.getString("parent_uid").isEmpty()) {
                 rsParentTable = Config.getDBBuilder(getProteu()).selectTable("", "",
-                        getProteu().getRequestAll().getString("parent_uid"));
+                        data.getString("parent_uid"));
             }
             Values user = null;
-            if (getProteu().getRequestAll().has("user") && !getProteu().getRequestAll().getString("user").isEmpty()) {
-                user = getItemById("netuno_user", getProteu().getRequestAll().getString("user"));
-            } else if (!getProteu().getRequestAll().getString("user_uid").isEmpty()) {
-                user = getItemByUId("netuno_user", getProteu().getRequestAll().getString("user_uid"));
+            if (data.has("user") && !data.getString("user").isEmpty()) {
+                user = getItemById("netuno_user", data.getString("user"));
+            } else if (!data.getString("user_uid").isEmpty()) {
+                user = getItemByUId("netuno_user", data.getString("user_uid"));
             }
             Values group = null;
-            if (getProteu().getRequestAll().has("group") && !getProteu().getRequestAll().getString("group").isEmpty()) {
-                group = getItemById("netuno_group", getProteu().getRequestAll().getString("group"));
-            } else if (!getProteu().getRequestAll().getString("group_uid").isEmpty()) {
-                group = getItemByUId("netuno_group", getProteu().getRequestAll().getString("group_uid"));
+            if (data.has("group") && !data.getString("group").isEmpty()) {
+                group = getItemById("netuno_group", data.getString("group"));
+            } else if (!data.getString("group_uid").isEmpty()) {
+                group = getItemByUId("netuno_group", data.getString("group_uid"));
             }
             Values values = new Values();
-            values.set("name", "'" + DB.sqlInjection(getProteu().getRequestAll().getString("name")) + "'");
-            values.set("title", "'" + DB.sqlInjection(getProteu().getRequestAll().getString("title")) + "'");
-            values.set("description", "'" + DB.sqlInjection(getProteu().getRequestAll().getString("description")) + "'");
+            values.set("name", "'" + DB.sqlInjection(data.getString("name")) + "'");
+            values.set("title", "'" + DB.sqlInjection(data.getString("title")) + "'");
+            values.set("description", "'" + DB.sqlInjection(data.getString("description")) + "'");
             values.set("parent_id",
                     rsParentTable != null && rsParentTable.size() == 1 ? rsParentTable.get(0).getString("id") : "0");
-            if (getProteu().getRequestAll().getBoolean("report")) {
-                values.set("report_behaviour", getProteu().getRequestAll().getInt("behaviour", 0));
+            if (data.getBoolean("report")) {
+                values.set("report_behaviour", data.getInt("behaviour", 0));
             }
             values.set("user_id", user != null ? user.getString("id") : "0");
             values.set("group_id", group != null ? group.getString("id") : "0");
-            values.set("show_id", getProteu().getRequestAll().getBoolean("show_id"));
-            values.set("control_active", getProteu().getRequestAll().getBoolean("control_active"));
-            values.set("control_user", getProteu().getRequestAll().getBoolean("control_user"));
-            values.set("control_group", getProteu().getRequestAll().getBoolean("control_group"));
-            values.set("export_xls", getProteu().getRequestAll().getBoolean("export_xls"));
-            values.set("export_xml", getProteu().getRequestAll().getBoolean("export_xml"));
-            values.set("export_json", getProteu().getRequestAll().getBoolean("export_json"));
-            values.set("export_id", getProteu().getRequestAll().getBoolean("export_id"));
-            values.set("export_uid", getProteu().getRequestAll().getBoolean("export_uid"));
-            values.set("export_lastchange", getProteu().getRequestAll().getBoolean("export_lastchange"));
-            values.set("report", getProteu().getRequestAll().getBoolean("report"));
-            values.set("reorder", getProteu().getRequestAll().getInt("reorder") > 0 ? getProteu().getRequestAll().getInt("reorder") : 0);
-            values.set("firebase", "'" + DB.sqlInjection(getProteu().getRequestAll().getString("firebase")) + "'");
+            values.set("show_id", data.getBoolean("show_id"));
+            values.set("control_active", data.getBoolean("control_active"));
+            values.set("control_user", data.getBoolean("control_user"));
+            values.set("control_group", data.getBoolean("control_group"));
+            values.set("export_xls", data.getBoolean("export_xls"));
+            values.set("export_xml", data.getBoolean("export_xml"));
+            values.set("export_json", data.getBoolean("export_json"));
+            values.set("export_id", data.getBoolean("export_id"));
+            values.set("export_uid", data.getBoolean("export_uid"));
+            values.set("export_lastchange", data.getBoolean("export_lastchange"));
+            values.set("report", data.getBoolean("report"));
+            values.set("reorder", data.getInt("reorder") > 0 ? data.getInt("reorder") : 0);
+            values.set("firebase", "'" + DB.sqlInjection(data.getString("firebase")) + "'");
 
             getExecutor().execute("update netuno_table set " + values.toString(", ", " = ") + " where id = "
-                    + DB.sqlInjectionInt(getProteu().getRequestAll().getString("id")) + ";");
+                    + DB.sqlInjectionInt(data.getString("id")) + ";");
 
             new org.netuno.tritao.resource.Setup(getProteu(), getHili()).autoCreateSchema();
 
@@ -209,12 +213,16 @@ public interface TableOperations extends BuilderBase, TableSelect, TableDesignSe
     }
 
     default boolean deleteTable() {
+        return deleteTable(getProteu().getRequestAll());
+    }
+
+    default boolean deleteTable(Values data) {
         boolean result = false;
         try {
-            List<Values> rsTableTritao = selectTable(getProteu().getRequestAll().getString("id"), "", "");
+            List<Values> rsTableTritao = selectTable(data.getString("id"), "", "");
             if (rsTableTritao.size() == 1) {
                 Values table = rsTableTritao.get(0);
-                if (!getProteu().getRequestAll().getBoolean("report")) {
+                if (!data.getBoolean("report")) {
                     String trashName = table.getString("name") + "_"
                             + new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
                     new Table(getBuilder()).rename(table.getString("name"), trashName);
