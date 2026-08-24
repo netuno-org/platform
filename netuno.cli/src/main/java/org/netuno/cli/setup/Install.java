5e3a6e7301e724e9200859deb5b2dcdaea5a370e
Eduardo Velasques <eduveks@gmail.com>
2026-08-24 13:53:59 +0100

    CLI: Install path avoids null pointer exception.

--- netuno.cli/src/main/java/org/netuno/cli/setup/Install.java
+++ netuno.cli/src/main/java/org/netuno/cli/setup/Install.java
@@ -82,14 +82,18 @@ public class Install implements MainArg {
     private final String checksumFileName = ".checksum.json";
 
     public void run() throws IOException, InterruptedException {
-        if (path.isBlank()) {
+        if (path == null || path.isBlank()) {
             path = Config.getHome();
         } else {
             Config.setHome(path);
         }
+        java.io.File homePathFile = new java.io.File(path).getAbsoluteFile();
+        if (path.equalsIgnoreCase(Config.HOME_DEFAULT)) {
+            homePathFile = homePathFile.getParentFile();
+        }
         System.out.println();
         System.out.println(OS.consoleOutput("@|white All will be set up in the directory: |@ "));
-        System.out.println(OS.consoleOutput("@|yellow "+ (path.equalsIgnoreCase(Config.HOME_DEFAULT) ? new java.io.File(path).getParentFile() : new java.io.File(path)).getAbsolutePath() +" |@ "));
+        System.out.println(OS.consoleOutput("@|yellow "+ homePathFile.getAbsolutePath() +" |@ "));
         System.out.println();
         try (Scanner scanner = new Scanner(System.in)) {
             if (remove) {
