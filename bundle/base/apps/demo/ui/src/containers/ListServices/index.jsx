063a683ea02a2bfa6ec887efa630e90b652a768a
Eduardo Velasques <eduveks@gmail.com>
2026-08-27 17:56:27 +0100

    Bundle: Demo app with TypeScript service samples added besides CSV and MongoDB.

--- bundle/base/apps/demo/ui/src/containers/ListServices/index.jsx
+++ bundle/base/apps/demo/ui/src/containers/ListServices/index.jsx
@@ -49,7 +49,7 @@ function ListServices() {
                     params={{
                       hours: 100
                     }}
-                    langs={["javascript", "kotlin"]}
+                    langs={["javascript", "typescript", "kotlin"]}
                   />
                 </li>
                 <li>
@@ -57,7 +57,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.date-format.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.date-format.intro` })}
                     name="date-format"
-                    langs={["javascript", "kotlin"]}
+                    langs={["javascript", "typescript", "kotlin"]}
                   />
                 </li>
                 <li>
@@ -65,7 +65,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.export-excel.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.export-excel.intro` })}
                     name="export-excel"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -73,7 +73,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.export-pdf.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.export-pdf.intro` })}
                     name="export-pdf"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -81,7 +81,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.user.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.user.intro` })}
                     name="user"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -89,7 +89,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.group.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.group.intro` })}
                     name="group"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
               </ul>
@@ -108,7 +108,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.mail-send.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.mail-send.intro` })}
                     name="mail-send"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -116,7 +116,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.print-lines.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.print-lines.intro` })}
                     name="print-lines"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -124,7 +124,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.print-template.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.print-template.intro` })}
                     name="print-template"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -132,7 +132,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.image.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.image.intro` })}
                     name="image"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -144,7 +144,7 @@ function ListServices() {
                       id: 1,
                       name: 'atriz'
                     }}
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -152,7 +152,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.db.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.db.intro` })}
                     name="db"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
               </ul>
@@ -167,7 +167,7 @@ function ListServices() {
                     params={{
                       id: 1
                     }}
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -175,7 +175,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.query-result.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.query-result.intro` })}
                     name="query-result"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -183,7 +183,15 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.uid.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.uid.intro` })}
                     name="uid"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
+                  />
+                </li>
+                <li>
+                  <ServiceSample
+                    title={intl.formatMessage({ id: `${messages}.services.mongo.title` })}
+                    intro={intl.formatMessage({ id: `${messages}.services.mongo.intro` })}
+                    name="mongo"
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -191,15 +199,15 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.upload.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.upload.intro` })}
                     name="upload"
-                    langs={["javascript", "kotlin"]}
+                    langs={["javascript", "typescript", "kotlin"]}
                   />
                 </li>
                 <li>
                   <ServiceSample
-                    title={intl.formatMessage({ id: `${messages}.services.remote-delete-json.title` })}
-                    intro={intl.formatMessage({ id: `${messages}.services.remote-delete-json.intro` })}
-                    name="remote-delete-json"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    title={intl.formatMessage({ id: `${messages}.services.csv.title` })}
+                    intro={intl.formatMessage({ id: `${messages}.services.csv.intro` })}
+                    name="csv"
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
               </ul>
@@ -208,10 +216,18 @@ function ListServices() {
               <ul className="list-services__list">
                 <li>
                   <ServiceSample
+                    title={intl.formatMessage({ id: `${messages}.services.remote-delete-json.title` })}
+                    intro={intl.formatMessage({ id: `${messages}.services.remote-delete-json.intro` })}
+                    name="remote-delete-json"
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
+                  />
+                </li>
+                <li>
+                  <ServiceSample
                     title={intl.formatMessage({ id: `${messages}.services.remote-patch-json.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.remote-patch-json.intro` })}
                     name="remote-patch-json"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -219,7 +235,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.remote-post-json.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.remote-post-json.intro` })}
                     name="remote-post-json"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -227,7 +243,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.remote-put-json.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.remote-put-json.intro` })}
                     name="remote-put-json"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -235,7 +251,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.remote-get-json.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.remote-get-json.intro` })}
                     name="remote-get-json"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
                 <li>
@@ -243,7 +259,7 @@ function ListServices() {
                     title={intl.formatMessage({ id: `${messages}.services.remote-mailjet-sms.title` })}
                     intro={intl.formatMessage({ id: `${messages}.services.remote-mailjet-sms.intro` })}
                     name="remote-mailjet-sms"
-                    langs={["groovy", "javascript", "kotlin", "python", "ruby"]}
+                    langs={["javascript", "typescript", "python", "ruby", "kotlin", "groovy"]}
                   />
                 </li>
               </ul>
